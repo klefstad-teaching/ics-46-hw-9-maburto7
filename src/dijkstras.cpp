c8b542710f68caa81131ae9742d71ef81769c4d0
Mia Desiree Aburto <maburto@uci.edu>
2025-03-16 17:15:50 +0000

    fixed a syntax issue

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -42,7 +42,6 @@ vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector
 
 
 void print_path(const vector<int>& v, int total){
-    cout<<"HELLO?"
     
     for (size_t i = 0; i < v.size(); ++i) {
         cout << v[i];
